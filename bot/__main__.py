4e1c03d131e341e9337093dd7ad0960cf5306c51
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-09 13:17:39 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/arshsisodiya/helios-mirror")
-    buttons.buildbutton("Support Group", "https://t.me/mirrorsociety")
+    buttons.buildbutton("Repo", "https://github.com/arkonn/Ark-Mirror")
+    buttons.buildbutton("Join Group", "https://t.me/arkmirror")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
