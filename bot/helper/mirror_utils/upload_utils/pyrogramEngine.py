5cb75fbdb655353b87b7c12e2e11bd3101d0cf01
sachin-sar <107174628+sachin-sar@users.noreply.github.com>
2022-06-15 20:30:00 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -154,7 +154,7 @@ class TgUploader:
                     notMedia = True
             if self.__as_doc or notMedia:
                 if file_.upper().endswith(VIDEO_SUFFIXES) and thumb is None:
-                    thumb = take_ss(up_path)
+                    pass
                     if self.__is_cancelled:
                         if self.__thumb is None and thumb is not None and ospath.lexists(thumb):
                             osremove(thumb)
